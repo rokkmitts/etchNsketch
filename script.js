93cb980cbc2f1a5d450b8c7067d9061ca005be1d
rokkmitts <jtc.copeland@outlook.com>
2023-07-31 22:51:42 -0400

    cleaned up the start of the js file by better structuring the the variables. (top to botton of js)-- built a formPackage function that sets a DOM element to an exisiting html one and based upon its value the function will initiate a loop to build a grid. If the inital conditions are not met, then the user will receive a error message instead. The next build is a clearPackage function that removes all child elements from the parent IF they exist.

--- script.js
+++ script.js
@@ -1,42 +1,58 @@
 "use strict";
 
-//creating DOM elements
+let realNum;
+//message DOM elements
 const msgUser = document.createElement("p");
-// connecting exisint DOM element
+// connecting existing elements
 const pixelPackage = document.getElementById("grid-container");
 const inputPackage = document.getElementById("input-container");
 const msgPackage = document.getElementById("msg-container");
 const refreshBtn = document.getElementById("refresh-btn");
 const submitBtn = document.getElementById("submit-btn");
-const numInput = document.getElementById("num-input").value;
-
-// button eventlisteners to call functions in sequence
-refreshBtn.addEventListener("click", function () {});
-submitBtn.addEventListener("click", function () {});
-
+//
+//builds pixel panel based on numberInput
+function formPackage() {
+  const numInput = document.getElementById("num-input").value;
+  if (numInput > 0 && numInput < 40) {
+    for (let x = 0; x < numInput; x++) {
+      for (let y = 0; y < numInput; y++) {
+        const divPixel = document.createElement("div");
+        divPixel.classList.add("divPixel");
+        pixelPackage.style.gridTemplateColumns = `repeat(${numInput}, auto)`;
+        pixelPackage.style.gridTemplateRows = `repeat(${numInput}, auto)`;
+        pixelPackage.appendChild(divPixel);
+      }
+    }
+  } else {
+    msgUser.textContent = `${numInput} is not valid try again`;
+    msgPackage.append(msgUser);
+  }
+}
+//
+//
+//removes all content in parent containers
 function clearPackage(parent) {
   while (parent.firstChild) {
     parent.removeChild(parent.firstChild);
   }
+  console.log("contents removed");
 }
 
-//builds pixel panel based on numberInput
-function formPackage() {
-  for (let x = 1; x < numInput; x++) {
-    for (let y = 1; y < numInput; y++) {
-      const divPixel = document.createElement("div");
-      pixelPackage.style.gridTemplateColumns = `repeat(${numInput}, auto)`;
-      pixelPackage.style.gridTemplateRows = `repeat(${numInput}, auto)`;
-      pixelPackage.appendChild(divPixel);
-    }
-  }
-  console.log("formPackage working");
+// buttons
+refreshBtn.addEventListener("click", function () {
+  clearPackage(pixelPackage);
+});
+submitBtn.addEventListener("click", function () {
+  clearPackage(pixelPackage);
+  formPackage();
+});
+
+//color generating function
+function colorGen() {
+  const red = parseInt(Math.random() * 255);
+  const green = parseInt(Math.random() * 255);
+  const blue = parseInt(Math.random() * 255);
+  console.log(`${red} \n ${green} \n ${blue}`);
 }
 
-//
-function checkInput() {
-  if (numInput > 0 && numInput < 50) {
-    msgUser.innerText = `${numInput} is a invalid entry. Try again`;
-    msgPackage.appendChild(msgUser);
-  }
-}
+colorGen();
