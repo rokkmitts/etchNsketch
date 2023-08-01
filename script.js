0d427db394e1d2bb03b154def08bf093993f70da
rokkmitts <jtc.copeland@outlook.com>
2023-07-31 23:52:32 -0400

    the formPackage function bugged out when i made its divPixel element global. so, i moved it back and built colorChange events within it. overall the function is bulky but it works. It holds two additional event listeners; one to initate the color change on mouse enter and one to keep it when mouse exits.

--- script.js
+++ script.js
@@ -4,13 +4,14 @@ let realNum;
 //message DOM elements
 const msgUser = document.createElement("p");
 // connecting existing elements
+// const divPixel = document.createElement("div");
 const pixelPackage = document.getElementById("grid-container");
 const inputPackage = document.getElementById("input-container");
 const msgPackage = document.getElementById("msg-container");
 const refreshBtn = document.getElementById("refresh-btn");
 const submitBtn = document.getElementById("submit-btn");
 //
-//builds pixel panel based on numberInput
+//builds pixel panel based on numberInput and gives mouse movement properties
 function formPackage() {
   const numInput = document.getElementById("num-input").value;
   if (numInput > 0 && numInput < 40) {
@@ -21,6 +22,14 @@ function formPackage() {
         pixelPackage.style.gridTemplateColumns = `repeat(${numInput}, auto)`;
         pixelPackage.style.gridTemplateRows = `repeat(${numInput}, auto)`;
         pixelPackage.appendChild(divPixel);
+
+        divPixel.addEventListener("mouseenter", function () {
+          divPixel.style = "background-color: rgb(0,0,0)";
+        });
+
+        divPixel.addEventListener("mouseleave", function () {
+          divPixel.style = "background-color: rgb(0,0,0)";
+        });
       }
     }
   } else {
@@ -46,13 +55,3 @@ submitBtn.addEventListener("click", function () {
   clearPackage(pixelPackage);
   formPackage();
 });
-
-//color generating function
-function colorGen() {
-  const red = parseInt(Math.random() * 255);
-  const green = parseInt(Math.random() * 255);
-  const blue = parseInt(Math.random() * 255);
-  console.log(`${red} \n ${green} \n ${blue}`);
-}
-
-colorGen();
